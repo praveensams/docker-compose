9a42f0b7c335b8e97d0ac6b536d92448dff7ea34
root <root@localhost.localdomain>
2018-03-20 00:14:57 +0000

    test

--- sam.sh
+++ sam.sh
@@ -1,7 +1,9 @@
- docker-compose up -d --force-recreate
+#!/bin/bash
+
+docker-compose up -d --force-recreate
  
- docker cp sshserver1:/tmp/authorized_keys .
+docker cp sshserver1:/tmp/authorized_keys .
   
- docker cp authorized_keys  sshserver2:/home/docker_root/.ssh/
+docker cp authorized_keys  sshserver2:/home/docker_root/.ssh/
    
    
